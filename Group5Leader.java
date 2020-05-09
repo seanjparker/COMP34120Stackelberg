5c00ae95d34c46ad1eb5df70c7ba7a58fc2b371c
Sean Parker <sean.parker067@gmail.com>
2020-05-09 14:25:20 +0100

    Get ready for submit

--- Group5Leader.java
+++ Group5Leader.java
@@ -1,4 +1,4 @@
-package comp34120.ex2;
+import comp34120.ex2.*;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -7,15 +7,59 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * A very simple leader implementation that only generates random prices
- *
- * @author Xin
- */
 final class Group5Leader extends PlayerImpl {
+    class LinearRegressionSolver {
+        private float a, b;
+
+        public LinearRegressionSolver() {
+            a = 1.0f;
+            b = 1.0f;
+        }
+
+        public float getA() {
+            return this.a;
+        }
+
+        public float getB() {
+            return this.b;
+        }
+
+        // Implements the equations in Lecture 4, slide 25
+        public void fit(ArrayList<Record> history) {
+            float leader_squared_sum = 0, follower_reaction_sum = 0, leader_sum = 0, leader_prod_follower_sum = 0;
+            int T = history.size(), window = 60;
+            Record day;
+
+            double forgetting_factor = 0.95;
+            // Single loop where we calculate the terms of the equations
+            for (int i = T - window - 1; i < T; i++) {
+                day = history.get(i);
+
+                leader_squared_sum += Math.pow(forgetting_factor, T-i)*(day.m_leaderPrice * day.m_leaderPrice);
+                follower_reaction_sum += Math.pow(forgetting_factor, T-i)*day.m_followerPrice;
+                leader_sum += Math.pow(forgetting_factor, T-i)*day.m_leaderPrice;
+                leader_prod_follower_sum += Math.pow(forgetting_factor, T-i)*(day.m_leaderPrice * day.m_followerPrice);
+            }
+
+            // Calculate the numerator for each of the equations
+            float a_numerator = (leader_squared_sum * follower_reaction_sum) - (leader_sum * leader_prod_follower_sum);
+            float b_numerator = (T * leader_prod_follower_sum) - (leader_sum * follower_reaction_sum);
+
+            // The denominator is the same in both of the equations
+            float denominator = (T * leader_squared_sum) - (leader_sum * leader_sum);
+
+            a = a_numerator / denominator;
+            b = b_numerator / denominator;
+        }
+
+        // Predict using the learnt parameters and the linear function
+        public float predict(float u) {
+            return a + u * b;
+        }
+    }
 
     private ArrayList<Record> platformHistory;
-    private Solver linearRegSolver;
+    private LinearRegressionSolver linearRegSolver;
 
     private Group5Leader() throws RemoteException, NotBoundException {
         super(PlayerType.LEADER, "Group5Leader");
@@ -41,7 +85,7 @@ final class Group5Leader extends PlayerImpl {
     	// Get last record
 		Record lastDay = m_platformStub.query(m_type, p_date - 1);
         platformHistory.add(lastDay);
-		float lastDayProfit = PlatformHelper.calculateProfit(lastDay);
+		float lastDayProfit = calculateProfit(lastDay);
 
 		// Train
         // Calculate parameters for followers reaction function
@@ -73,7 +117,7 @@ final class Group5Leader extends PlayerImpl {
             float total = 0.0f;
             for (int i = 100; i <= 129; i++) {
                 // It only matters about the profit for the last 30 days in the simulation
-                total += PlatformHelper.calculateProfit(platformHistory.get(i));
+                total += calculateProfit(platformHistory.get(i));
             }
 
             m_platformStub.log(PlayerType.LEADER, "Total profit = " + total);
@@ -83,8 +127,21 @@ final class Group5Leader extends PlayerImpl {
     @Override
     public void startSimulation(int p_steps) throws RemoteException {
         platformHistory = new ArrayList<Record>();
-        platformHistory = PlatformHelper.getRecordHistory(m_platformStub);
+        platformHistory = getRecordHistory(m_platformStub);
 		linearRegSolver = new LinearRegressionSolver();
+    }
+
+    public static ArrayList<Record> getRecordHistory(Platform plt) throws RemoteException {
+        ArrayList<Record> history = new ArrayList<Record>();
+        for (int i = 1; i <= 100; i++) {
+            history.add(plt.query(PlayerType.LEADER, i));
+        }
+        return history;
+    }
+
+    public static float calculateProfit(Record day) {
+        // (u_L - c_L) * S_L(u_L, u_F)
+        return (day.m_leaderPrice - day.m_cost) * (2 - day.m_leaderPrice + (0.3f * day.m_followerPrice));
     }
 
     /**
--- src/main/java/comp34120/ex2/ProfitMaximiser.java
+++
@@ -1,9 +0,0 @@
-package comp34120.ex2;
-
-public class ProfitMaximiser {
-
-    public float getOptimalPrice() {
-        // Determine our leader price based on the predicted price of the follower
-        return 1.0f;
-    }
-}
